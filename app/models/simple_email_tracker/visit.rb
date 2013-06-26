c414998ab37e53712aa842dd5ade054d925ba516
Komal Dhanwani <komal@complitech.net>
2013-06-26 13:10:17 +0530

    first visit issue and track country

--- app/models/simple_email_tracker/visit.rb
+++ app/models/simple_email_tracker/visit.rb
@@ -11,12 +11,25 @@ module SimpleEmailTracker
       et = self.find_or_create_by_key key
     end
 
+    def get_country_from_ip(request_ip)
+      location = GeoIp.geocode_ip(request_ip)
+      if location.present?
+        [location.country_code2, location.country_name]
+      else
+        [nil,nil]
+      end
+    end
+
+
     def visit_by request
+      country = get_country_from_ip(request_ip)
       now = Time.now
       self.count += 1
       self.first_visited_at = now unless self.first_visited_at
       self.last_visited_at = now
       self.ip = request.ip
+      self.country_code = country[0]
+      self.country_name = country[1]
       self.user_agent = request.env["HTTP_USER_AGENT"]
       self.save
     end
