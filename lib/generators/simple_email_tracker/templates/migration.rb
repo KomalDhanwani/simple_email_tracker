6b559591f5cc7f4a5930d536d9e3fb1cad66cb76
Komal Dhanwani <komal@complitech.net>
2013-06-26 14:07:27 +0530

    add coulumns

--- lib/generators/simple_email_tracker/templates/migration.rb
+++ lib/generators/simple_email_tracker/templates/migration.rb
@@ -8,6 +8,8 @@ class CreateSimpleEmailTrackerTables < ActiveRecord::Migration
       t.string :user_agent
       t.datetime :first_visited_at
       t.datetime :last_visited_at
+      t.string :country_code
+      t.string :country_name
 
       t.timestamps
     end
